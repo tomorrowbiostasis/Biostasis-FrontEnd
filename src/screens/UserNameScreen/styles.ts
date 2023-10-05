de8934803812408701745e007b29acb2a1c005a8
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 15:52:51 +0200

    Implemented new design for the screen

--- src/screens/UserNameScreen/styles.ts
+++ src/screens/UserNameScreen/styles.ts
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import colors from '~/theme/colors';
 
 const styles = StyleSheet.create({
   contentContainer: {
@@ -13,10 +14,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   submitButton: {
-    marginTop: 100,
-  },
-  container: {
-    paddingTop: 0,
+    marginTop: 40,
   },
   content: {
     marginBottom: 3,
@@ -24,6 +22,44 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: '10%',
     width: '100%',
+  },
+  container: {
+    padding: 20,
+  },
+  panel: {
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    marginVertical: 10,
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  panelHeader: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginVertical: 10,
+  },
+  panelBody: {
+    flex: 1,
+    width: '100%',
+    marginVertical: 10,
+  },
+  panelFooter: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'flex-start',
+    marginTop: 10,
+  },
+  lineStyle: {
+    flex: 1,
+    width: '95%',
+    borderBottomColor: colors.gray[300],
+    borderBottomWidth: 2,
+    height: 2,
+  },
+  icon: {
+    marginRight: 10,
   },
 });
 
