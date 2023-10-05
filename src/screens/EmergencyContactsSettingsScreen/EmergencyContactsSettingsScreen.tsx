52b834148be87930d651dace1dd50df867e05953
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 14:15:16 +0200

    Modified EmergencyContactsSettingsScreen - Added Emergency contact list card where users can add emergency contacts. - Added Documents card which allows users to upload the documents that they want to send to their emergency contacts. - Added EmergencyMessage card which allows the users to customize the type of message their contacts will receive incase of emergency situation.

--- src/screens/EmergencyContactsSettingsScreen/EmergencyContactsSettingsScreen.tsx
+++ src/screens/EmergencyContactsSettingsScreen/EmergencyContactsSettingsScreen.tsx
@@ -1,196 +1,34 @@
-import {Formik, FormikProps} from 'formik';
-import {Button, Text, View} from 'native-base';
-import React, {useCallback, useEffect} from 'react';
+import React from 'react';
+import {ScrollView} from 'react-native';
 import Container from '~/components/Container';
-import Loader from '~/components/Loader';
-import TextArea from '~/components/TextArea';
+import EmergencyContactsList from './components/EmergencyContactsList';
 import {useAppTranslation} from '~/i18n/hooks/UseAppTranslation.hook';
-import {useAppDispatch, useAppSelector} from '~/redux/store/hooks';
-import {
-  EmergencyButtonSettings,
-  selectEmergencyButtonSettings,
-  testMessageSelector,
-  userSelector,
-} from '~/redux/user/selectors';
-
-import {setEmergencyButtonSettingsUpdated} from '~/redux/user/user.slice';
-import {
-  sendTestMessage,
-  updateEmergencyButtonSettings,
-} from '~/redux/user/thunks';
-import {useEmergencyContactSettingsValidationSchema} from '~/services/Validation.service';
-import SwitchButton from '~/components/SwitchButton';
-import {updateDataCollectionStatus} from '~/utils';
 import styles from './styles';
-
-import {getLocation} from '~/services/Location.service';
-import {useMemo} from 'react';
+import Documents from './components/Documents';
+import EmergencyMessage from './components/EmergencyMessage/EmergencyMessage';
+import {Box} from 'native-base';
 
 const EmergencyContactsSettingsScreen = () => {
   const {t} = useAppTranslation();
-  const dispatch = useAppDispatch();
-
-  const emergencyContactSettingsValidationSchema =
-    useEmergencyContactSettingsValidationSchema();
-
-  const {pending, emergencyButtonSettingsUpdated, user} =
-    useAppSelector(userSelector);
-  const emergencyButtonSettings = useAppSelector(selectEmergencyButtonSettings);
-  const testMessage = useAppSelector(testMessageSelector);
-
-  useEffect(() => {
-    return () => {
-      dispatch(setEmergencyButtonSettingsUpdated(false));
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [emergencyButtonSettingsUpdated]);
-
-  const initialFormValues = useMemo(() => {
-    const init = emergencyButtonSettings;
-    if (!init.emergencyMessage) {
-      init.emergencyMessage = t('emergencyContacts.settings.defaultMessage', {
-        username: user.name,
-      });
-    }
-    return init;
-  }, [emergencyButtonSettings, t, user.name]);
-
-  const handleConfirm = useCallback(
-    (values: EmergencyButtonSettings) => {
-      dispatch(updateEmergencyButtonSettings(values)).then(() => {
-        updateDataCollectionStatus();
-      });
-    },
-    [dispatch],
-  );
-
-  const handleLocationSettingsChange = useCallback(
-    async (
-      value: boolean,
-      setFieldValue: FormikProps<EmergencyButtonSettings>['setFieldValue'],
-    ) => {
-      setFieldValue('locationAccess', value);
-      if (value) {
-        try {
-          await getLocation();
-        } catch (error) {
-          setFieldValue('locationAccess', false);
-        }
-      }
-    },
-    [],
-  );
-
-  const handleSendTestEmailPress = useCallback(async () => {
-    dispatch(sendTestMessage());
-  }, [dispatch]);
-
-  const hasInitialStateChanged = useCallback(
-    (values: EmergencyButtonSettings) => {
-      const keys = Object.keys(
-        values,
-      ) as unknown as (keyof EmergencyButtonSettings)[];
-
-      for (let key of keys) {
-        if (values[key] !== initialFormValues[key]) {
-          return true;
-        }
-      }
-      return false;
-    },
-    [initialFormValues],
-  );
 
   return (
     <Container
-      title={t('emergencyContacts.emergencyButtonSettings')}
-      type={'keyboardAvoidingScrollView'}
-      contentContainerStyle={styles.content}>
-      {emergencyButtonSettings.emergencyEmailAndSms == null ? (
-        <Loader />
-      ) : (
-        <Formik<EmergencyButtonSettings>
-          initialValues={initialFormValues}
-          onSubmit={handleConfirm}
-          validationSchema={emergencyContactSettingsValidationSchema}
-          validateOnBlur={true}
-          validateOnChange={true}>
-          {({
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            values,
-            touched,
-            errors,
-            setFieldValue,
-            isValid,
-          }) => {
-            return (
-              <>
-                <View>
-                  <Text style={styles.headerText}>
-                    {t('emergencyContacts.settings.disclaimer')}
-                  </Text>
-                  <Text
-                    style={[styles.headerText, styles.includeInfoHeaderText]}>
-                    {t('emergencyContacts.settings.includeInfo')}
-                  </Text>
-                  <SwitchButton
-                    value={values.locationAccess}
-                    onSwitchPress={value =>
-                      handleLocationSettingsChange(value, setFieldValue)
-                    }
-                    containerStyle={styles.switchButton}
-                    title={t('emergencyContacts.settings.location')}
-                  />
-                  <SwitchButton
-                    value={values.uploadedDocumentsAccess}
-                    onSwitchPress={value =>
-                      setFieldValue('uploadedDocumentsAccess', value)
-                    }
-                    containerStyle={styles.switchButton}
-                    title={t('emergencyContacts.settings.uploadedDocuments')}
-                  />
-                  <TextArea
-                    label={t('common.message')}
-                    onChangeText={handleChange('emergencyMessage')}
-                    onBlur={handleBlur('emergencyMessage')}
-                    value={values.emergencyMessage}
-                    errorMessage={errors.emergencyMessage}
-                    containerStyle={styles.messageAreaContainer}
-                    isValid={
-                      !errors.emergencyMessage && touched.emergencyMessage
-                    }
-                  />
-                </View>
-                <View>
-                  <Button
-                    style={styles.button}
-                    disabled={
-                      !isValid || pending || !hasInitialStateChanged(values)
-                    }
-                    isLoading={pending}
-                    onPress={handleSubmit}>
-                    {t('common.confirm')}
-                  </Button>
-                  <Button
-                    variant={'outline'}
-                    style={styles.button}
-                    disabled={
-                      !isValid ||
-                      testMessage.pending ||
-                      hasInitialStateChanged(values)
-                    }
-                    isLoading={testMessage.pending}
-                    onPress={handleSendTestEmailPress}>
-                    {t('emergencyContacts.settings.sendTestEmail')}
-                  </Button>
-                </View>
-              </>
-            );
-          }}
-        </Formik>
-      )}
+      title={t('emergencyContactsSettings.title')}
+      containerStyle={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      disableWrapper
+      showBackIcon
+      showDrawerIcon>
+      <Box style={styles.curveElement} />
+
+      <ScrollView
+        bounces={false}
+        style={styles.scrollContent}
+        contentContainerStyle={styles.scrollContentContainer}>
+        <EmergencyContactsList />
+        <Documents />
+        <EmergencyMessage />
+      </ScrollView>
     </Container>
   );
 };
--- src/screens/EmergencyContactsSettingsScreen/styles.ts
+++ src/screens/EmergencyContactsSettingsScreen/styles.ts
@@ -1,29 +1,71 @@
 import {StyleSheet} from 'react-native';
+import boxShadow from '~/theme/boxShadow';
 import colors from '~/theme/colors';
 
 const styles = StyleSheet.create({
-  headerText: {
-    fontSize: 14,
-    lineHeight: 20,
-    color: colors.gray[400],
-    marginBottom: 5,
+  container: {
+    flex: 1,
   },
-  switchButton: {
+  contentContainer: {
+    paddingTop: 80,
+  },
+  scrollContent: {
+    flex: 1,
+    width: '100%',
+  },
+  scrollContentContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  curveElement: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 50,
+    height: 50,
+    backgroundColor: colors.gray[50],
+    width: '100%',
+    borderBottomLeftRadius: 100,
+    borderBottomRightRadius: 100,
+  },
+  panel: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    marginVertical: 10,
+    padding: 20,
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    ...boxShadow,
+  },
+  panelHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginVertical: 10,
+  },
+  panelBody: {
+    flex: 1,
+    width: '100%',
+    marginVertical: 10,
+  },
+  panelFooter: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'flex-start',
     marginTop: 10,
   },
-  includeInfoHeaderText: {
-    marginTop: 25,
+  icon: {
+    marginRight: 10,
   },
-  messageAreaContainer: {
-    marginTop: 30,
-    marginBottom: 10,
+  lineStyle: {
+    width: '95%',
+    borderBottomColor: colors.gray[300],
+    borderBottomWidth: 1,
+    height: 1,
   },
-  button: {
-    marginTop: 20,
-  },
-  content: {
-    justifyContent: 'space-between',
-    flex: 1,
+  listContentContainer: {
+    flexGrow: 1,
   },
 });
 
