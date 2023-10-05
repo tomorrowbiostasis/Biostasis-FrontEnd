d56589a75c0d9c1ddc94cb2e94f22b4ab4c01649
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 12:13:07 +0200

    Modified the style of the LogoutTrigger component to match the new design

--- src/components/LogoutTrigger/styles.ts
+++ src/components/LogoutTrigger/styles.ts
@@ -1,19 +1,18 @@
 import {StyleSheet} from 'react-native';
+import colors from '~/theme/colors';
 
 const styles = StyleSheet.create({
   container: {
-    top: 1,
+    flex: 1,
   },
   row: {
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 2,
-  },
-  title: {
-    fontSize: 17,
   },
   icon: {
-    marginLeft: 10,
+    marginRight: 10,
+    transform: [{rotate: '180deg'}],
+    color: colors.red[400],
   },
 });
 
