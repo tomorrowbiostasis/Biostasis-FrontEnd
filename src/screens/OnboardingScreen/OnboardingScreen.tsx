f2b7d494e7ff37d7ef59f46570ce565e5f7bf622
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 14:21:39 +0200

    Implemented new design - Added login directly button. - Added new buttons Login /Sign up styles

--- src/screens/OnboardingScreen/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen/OnboardingScreen.tsx
@@ -1,14 +1,16 @@
-import React, {FC, useCallback, useState} from 'react';
-import {View, Image} from 'native-base';
+import React, {FC, useCallback, useMemo, useState} from 'react';
+import {View, Text} from 'native-base';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
 
 import OnboardingSlide from './components/OnboardingSlide';
 import OnboardingSummarySlide from './components/OnboardingSummarySlide';
 
-import colors from '~/theme/colors';
 import styles from './styles';
 import {useAppTranslation} from '~/i18n/hooks/UseAppTranslation.hook';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
+import {Screens} from '~/models/Navigation.model';
 
 const slideImage1 = require('~/assets/images/onboarding/OnboardingImage1.png');
 const slideImage2 = require('~/assets/images/onboarding/OnboardingImage2.png');
@@ -16,30 +18,31 @@ const slideImage2 = require('~/assets/images/onboarding/OnboardingImage2.png');
 const OnboardingScreen: FC = () => {
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const {t} = useAppTranslation();
+  const {navigate} = useNavigation();
 
-  const isLastSlide = useCallback(
+  const isNotLastSlide = useMemo(
     () => activeSlideIndex < 2,
     [activeSlideIndex],
   );
+
+  const handleLogin = useCallback(async () => {
+    //@ts-ignore
+    navigate(Screens.Auth, {action: 'SIGN_IN'});
+  }, [navigate]);
 
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeAreaContainer}>
         <Swiper
-          showsButtons={true}
           loop={false}
           dotStyle={styles.paginationDot}
-          activeDotStyle={styles.paginationDot}
-          activeDotColor={colors.sea[400]}
-          dotColor={colors.gray[350]}
-          onIndexChanged={index => setActiveSlideIndex(index)}
-          showsPagination={isLastSlide()}>
+          activeDotStyle={styles.activePaginationDot}
+          onIndexChanged={index => setActiveSlideIndex(index)}>
           <OnboardingSlide
             key={'slide-1'}
             image={slideImage1}
             label={t('onboarding.slide1.label')}
             text={t('onboarding.slide1.text')}
-            imageStyle={styles.slide1Image}
           />
           <OnboardingSlide
             key={'slide-2'}
@@ -49,6 +52,16 @@ const OnboardingScreen: FC = () => {
           />
           <OnboardingSummarySlide key={'slide-summary'} />
         </Swiper>
+        {isNotLastSlide && (
+          <View style={styles.alreadyUserBox}>
+            <Text fontSize={'md'}>{t('onboarding.alreadyUser')}</Text>
+            <TouchableOpacity onPress={handleLogin} style={styles.logInBox}>
+              <Text style={styles.logInText} underline>
+                {t('LogIn.LogIn')}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </SafeAreaView>
     </View>
   );
