e3e2b0c7b92f55cc08f69d88e21167ef4a539a29
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 12:58:59 +0200

    Created new design for the screens - Replace the only input text for the address with multiple inputs fields for different type of address details (Street-City-Country-zipCode). - Added a (Current Location) button in cases the user want to fill the fields automatically with their current location details.

--- src/screens/AddressScreen/AddressScreen.tsx
+++ src/screens/AddressScreen/AddressScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback} from 'react';
-import {Button, Text, View} from 'native-base';
+import React, {useCallback, useState} from 'react';
+import {Button, Spinner, Text, View} from 'native-base';
 import {Formik} from 'formik';
 import {useNavigation} from '@react-navigation/native';
 
@@ -10,13 +10,31 @@ import Input from '~/components/Input';
 
 import {useAppDispatch, useAppSelector} from '~/redux/store/hooks';
 import {updateUser} from '~/redux/user/thunks';
-import {userSelector} from '~/redux/user/selectors';
+import {userLoading, userSelector} from '~/redux/user/selectors';
 import {useUserAddressValidationSchema} from '~/services/Validation.service';
 
 import styles from './styles';
+import {Screens} from '~/models/Navigation.model';
+import IconEntypo from 'react-native-vector-icons/Entypo';
+import ToastService from '~/services/Toast.service';
+import {getLocation} from '~/services/Location.service';
+import axios from 'axios';
+import {TouchableOpacity} from 'react-native';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import colors from '~/theme/colors';
 
 type AddressFormFields = {
-  address: string;
+  street: string;
+  city: string;
+  country: string;
+  zipCode: string;
+};
+
+const initialValues: AddressFormFields = {
+  street: '',
+  city: '',
+  country: '',
+  zipCode: '',
 };
 
 export const AddressScreen = () => {
@@ -25,79 +43,216 @@ export const AddressScreen = () => {
   const {reset} = useNavigation();
   const {user} = useAppSelector(userSelector);
   const userAddressValidationSchema = useUserAddressValidationSchema();
+  const pending = useAppSelector(userLoading);
+  const [isLoadingLocation, setIsLoadingLocation] = useState(false);
+
+  const reverseGeoCode = async () => {
+    try {
+      const geoPosition = await getLocation();
+      const {latitude, longitude} = geoPosition.coords;
+
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`,
+      );
+      const data = response.data;
+      const addressComponents = data.address;
+
+      return addressComponents;
+    } catch (error) {
+      console.error(error);
+      return '';
+    }
+  };
+
+  const handleCurrentLocation = useCallback(async setFieldValue => {
+    try {
+      setIsLoadingLocation(true);
+      const address = await reverseGeoCode();
+      setFieldValue('street', address.road);
+      setFieldValue('city', address.city);
+      setFieldValue('country', address.country);
+      setFieldValue('zipCode', address.postcode);
+      setIsLoadingLocation(false);
+    } catch (error) {
+      setIsLoadingLocation(false);
+      console.error(error);
+    }
+  }, []);
 
   const handleContinuePress = useCallback(
-    async ({address}: AddressFormFields) => {
-      //TODO there is no error handling mechanism , add Notification.service.error for all update
-      dispatch(
-        updateUser({
-          address,
-        }),
-      ).then(() => {
-        /*
-          Moved here because of race condition - we cannot reset navigation while Home is not available for Navigator yet
-         */
-        reset({
-          index: 0,
-          routes: [{name: 'Home'}],
-        });
-      });
+    async (userAddress: AddressFormFields) => {
+      try {
+        const {street, city, country, zipCode} = userAddress;
+        const address = `${street}, ${city}, ${country}, ${zipCode}`;
+
+        await dispatch(
+          updateUser({
+            address,
+          }),
+        ).then(() =>
+          reset({
+            index: 0,
+            routes: [{name: Screens.Home as never}],
+          }),
+        );
+      } catch (error) {
+        ToastService.error(t('userAddress.wrongAddress'));
+      }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [dispatch, reset],
   );
+
   return (
     <Container
-      title={t('common.welcome', {username: user.name})}
       containerStyle={styles.container}
       contentContainerStyle={styles.contentContainer}
       type={'keyboardAvoidingScrollView'}>
-      <View style={styles.content}>
-        <View style={styles.textContainer}>
-          <Text style={styles.text} fontSize={'xl'}>
+      <View style={styles.panel}>
+        <Text style={styles.userName}>
+          {t('common.welcome', {username: user.name || 'user'})}
+        </Text>
+        <View style={styles.panelHeader}>
+          <IconEntypo name={'location'} size={26} style={styles.icon} />
+          <Text fontSize={'md'} textAlign={'center'} fontWeight={700}>
             {t('userAddress.title')}
           </Text>
         </View>
-
-        <Formik<AddressFormFields>
-          initialValues={{address: ''}}
-          onSubmit={handleContinuePress}
-          validationSchema={userAddressValidationSchema}
-          validateOnChange={true}>
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            touched,
-            errors,
-          }) => (
-            <>
-              <View style={styles.space}>
-                <Input
-                  type="address"
-                  label={t('userAddress.yourAddress')}
-                  onChangeText={handleChange('address')}
-                  onBlur={handleBlur('address')}
-                  placeholder={t('userAddress.placeholder')}
-                  value={values.address}
-                  errorMessage={
-                    errors.address && touched.address
-                      ? errors.address
-                      : undefined
+        <View style={styles.lineStyle} />
+        <View style={styles.panelBody}>
+          <Formik<AddressFormFields>
+            initialValues={initialValues}
+            onSubmit={handleContinuePress}
+            validationSchema={userAddressValidationSchema}
+            validateOnChange={true}>
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              setFieldValue,
+              values,
+              touched,
+              errors,
+            }) => (
+              <>
+                <View style={styles.panelFooter}>
+                  <Input
+                    type="street"
+                    label={t('userAddress.street')}
+                    onChangeText={handleChange('street')}
+                    onBlur={handleBlur('street')}
+                    mb={2}
+                    onSubmitEditing={() => {
+                      if (!errors.street) {
+                        handleSubmit();
+                      }
+                    }}
+                    value={values.street}
+                    errorMessage={
+                      errors.street && touched.street
+                        ? errors.street
+                        : undefined
+                    }
+                    isValid={!errors.street && touched.street}
+                  />
+                  <Input
+                    type="city"
+                    label={t('userAddress.city')}
+                    onChangeText={handleChange('city')}
+                    onBlur={handleBlur('city')}
+                    mb={2}
+                    onSubmitEditing={() => {
+                      if (!errors.city) {
+                        handleSubmit();
+                      }
+                    }}
+                    value={values.city}
+                    errorMessage={
+                      errors.city && touched.city ? errors.city : undefined
+                    }
+                    isValid={!errors.city && touched.city}
+                  />
+                  <Input
+                    type="country"
+                    label={t('userAddress.country')}
+                    onChangeText={handleChange('country')}
+                    onBlur={handleBlur('country')}
+                    mb={2}
+                    onSubmitEditing={() => {
+                      if (!errors.country) {
+                        handleSubmit();
+                      }
+                    }}
+                    value={values.country}
+                    errorMessage={
+                      errors.country && touched.country
+                        ? errors.country
+                        : undefined
+                    }
+                    isValid={!errors.country && touched.country}
+                  />
+                  <Input
+                    type="zipCode"
+                    keyboardType="numeric"
+                    label={t('userAddress.zipCode')}
+                    onChangeText={handleChange('zipCode')}
+                    onBlur={handleBlur('zipCode')}
+                    mb={2}
+                    onSubmitEditing={() => {
+                      if (!errors.zipCode) {
+                        handleSubmit();
+                      }
+                    }}
+                    value={values.zipCode}
+                    errorMessage={
+                      errors.zipCode && touched.zipCode
+                        ? errors.zipCode
+                        : undefined
+                    }
+                    isValid={!errors.zipCode && touched.zipCode}
+                  />
+                  <TouchableOpacity
+                    onPress={() => handleCurrentLocation(setFieldValue)}
+                    style={styles.getLocationBox}
+                    disabled={isLoadingLocation}>
+                    {isLoadingLocation ? (
+                      <Spinner color={colors.white} size="small" />
+                    ) : (
+                      <>
+                        <IconFontAwesome
+                          name={'location-arrow'}
+                          size={25}
+                          color={colors.white}
+                        />
+                        <Text fontSize={'sm'} color={colors.white} ml={2}>
+                          {t('userAddress.currentLocation')}
+                        </Text>
+                      </>
+                    )}
+                  </TouchableOpacity>
+                </View>
+                <Button
+                  variant={'solid'}
+                  disabled={
+                    !!errors.street ||
+                    !values.street ||
+                    !!errors.city ||
+                    !values.city ||
+                    !!errors.country ||
+                    !values.country ||
+                    !!errors.zipCode ||
+                    !values.zipCode ||
+                    pending
                   }
-                  isValid={!errors.address && touched.address}
-                />
-              </View>
-              <Button
-                variant={'solid'}
-                disabled={!!errors.address || !values.address}
-                style={styles.submitButton}
-                onPress={handleSubmit}>
-                {t('common.continue')}
-              </Button>
-            </>
-          )}
-        </Formik>
+                  isLoading={pending}
+                  style={styles.submitButton}
+                  onPress={() => handleSubmit()}>
+                  {t('common.continue')}
+                </Button>
+              </>
+            )}
+          </Formik>
+        </View>
       </View>
     </Container>
   );
--- src/screens/AddressScreen/styles.ts
+++ src/screens/AddressScreen/styles.ts
@@ -1,33 +1,66 @@
 import {StyleSheet} from 'react-native';
+import colors from '~/theme/colors';
 
 const styles = StyleSheet.create({
   contentContainer: {
-    paddingHorizontal: 0,
-  },
-  textContainer: {
-    alignItems: 'flex-end',
-    paddingBottom: 30,
-  },
-  text: {
-    textAlign: 'right',
-  },
-  space: {
-    marginBottom: 20,
-    marginTop: 20,
-    height: 50,
-  },
-  submitButton: {
-    marginTop: 80,
-  },
-  container: {
     paddingTop: 0,
   },
-  content: {
-    marginBottom: 3,
-    justifyContent: 'flex-end',
-    flex: 1,
-    paddingHorizontal: '10%',
+  submitButton: {
+    marginTop: 10,
+  },
+  container: {
+    padding: 20,
     width: '100%',
+  },
+  panel: {
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  panelHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  panelBody: {
+    flex: 1,
+    width: '100%',
+    marginVertical: 20,
+  },
+  panelFooter: {
+    marginVertical: 10,
+  },
+  lineStyle: {
+    flex: 1,
+    width: '95%',
+    borderBottomColor: colors.gray[300],
+    borderBottomWidth: 2,
+    height: 2,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    paddingVertical: 20,
+    color: colors.magenta[200],
+  },
+  getLocationBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.blue[600],
+    borderColor: colors.gray[50],
+    marginTop: 20,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    padding: 10,
   },
 });
 
