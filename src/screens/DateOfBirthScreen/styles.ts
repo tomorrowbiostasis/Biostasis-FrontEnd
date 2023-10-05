e75bc79bcb01dff75cf53bbc421515803cf6e4a7
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 14:10:42 +0200

    Modified the DateOfBirthScreen to match the new design

--- src/screens/DateOfBirthScreen/styles.ts
+++ src/screens/DateOfBirthScreen/styles.ts
@@ -1,28 +1,51 @@
 import {StyleSheet} from 'react-native';
+import colors from '~/theme/colors';
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    paddingHorizontal: 0,
-  },
   container: {
-    paddingTop: 0,
-  },
-  content: {
-    marginBottom: 3,
-    justifyContent: 'flex-end',
-    flex: 1,
-    paddingHorizontal: '10%',
+    padding: 20,
     width: '100%',
   },
-  text: {
-    alignItems: 'flex-end',
-    paddingBottom: 40,
-  },
   submitButton: {
-    marginTop: 80,
+    marginTop: 20,
   },
   inputContainer: {
     height: 60,
+  },
+  panel: {
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    marginVertical: 10,
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  panelHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginVertical: 10,
+  },
+  panelBody: {
+    flex: 1,
+    width: '100%',
+    marginVertical: 40,
+  },
+  lineStyle: {
+    flex: 1,
+    width: '95%',
+    borderBottomColor: colors.gray[300],
+    borderBottomWidth: 2,
+    height: 2,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    paddingVertical: 20,
+    color: colors.magenta[200],
   },
 });
 
