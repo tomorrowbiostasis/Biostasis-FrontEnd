ba0bcc73b82e762bf5e64b2e63bf776da15ed590
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-05 18:13:03 +0200

    Updated Push service to handle push notification - handleRemoteMessages(): messages and notifications received from the backend.

--- src/services/Push.service.ts
+++ src/services/Push.service.ts
@@ -1,85 +1,55 @@
-import {Alert} from 'react-native';
-import {
-  Notification,
-  Notifications,
-  Registered,
-  RegistrationError,
-} from 'react-native-notifications';
-import i18n from '~/i18n/i18n';
 import API from '~/services/API.service';
-import {NotificationTypesEnum} from '~/services/Background.types';
-import EnvConfig from '~/services/Env.service';
-import {isAndroid, isIOS, openSettings} from '~/utils';
+import {isAndroid, isIOS} from '~/utils';
 import {AsyncStorageService} from './AsyncStorage.service/AsyncStorage.service';
 import {AsyncStorageEnum} from './AsyncStorage.service/AsyncStorage.types';
 import messaging from '@react-native-firebase/messaging';
-
+import notifee, {AuthorizationStatus} from '@notifee/react-native';
+import {updateNotification} from './Notification.service';
+import {soundNotification, updateLocation} from './Background.service';
+import {NotificationTypesEnum} from '~/constants/notification.constants';
+import {AppState, Vibration} from 'react-native';
+import {navigationRef} from '~/navigators';
+import {Screens} from '~/models/Navigation.model';
 let initialized = false;
 
-export const listenForPushTokenAndUpdate = () => {
+export const listenForPushTokenAndUpdate = async () => {
   if (!initialized) {
-    Notifications.events().registerRemoteNotificationsRegistered(
-      async (event: Registered) => {
-        const currentSavedToken = await AsyncStorageService.getItem(
-          AsyncStorageEnum.LastSavedPushToken,
-        );
-        const fcmToken = await messaging().getToken();
-        console.log('+=========>>> UPDATED FCM ', fcmToken);
-        if (`${currentSavedToken}` !== event.deviceToken) {
-          let updatedToken = event.deviceToken;
-
-          if (isIOS) {
-            updatedToken = await messaging().getToken();
-          }
-          console.log('FCMTOKEN', updatedToken);
-
-          API.updateUserToken(updatedToken)
-            .then(() => {
-              console.log('successfully updated push token in api');
-
-              AsyncStorageService.setItem(
-                AsyncStorageEnum.LastSavedPushToken,
-                event.deviceToken,
-              ).then(() =>
-                console.log(
-                  'token saved locally at',
-                  AsyncStorageEnum.LastSavedPushToken,
-                ),
-              );
-            })
-            .catch(() => console.log('error updating push token in api'));
-        } else {
-          console.log('token did not change, doing nothing');
-        }
-      },
+    const currentSavedToken = await AsyncStorageService.getItem(
+      AsyncStorageEnum.LastSavedPushToken,
     );
+    const fcmToken = await messaging().getToken();
+    console.log('+=========>>> Updated FCM ', fcmToken);
+    if (currentSavedToken !== fcmToken) {
+      API.updateUserToken(fcmToken)
+        .then(() => {
+          console.log('successfully updated push token in api');
 
-    Notifications.events().registerRemoteNotificationsRegistrationFailed(
-      (event: RegistrationError) => {
-        console.warn('error while getting push token', event);
-      },
-    );
-
+          AsyncStorageService.setItem(
+            AsyncStorageEnum.LastSavedPushToken,
+            fcmToken,
+          ).then(() =>
+            console.log(
+              'token saved locally at',
+              AsyncStorageEnum.LastSavedPushToken,
+            ),
+          );
+        })
+        .catch(() => console.log('error updating push token in api'));
+    } else {
+      console.log('token did not change, doing nothing');
+    }
     initialized = true;
   }
 };
 
-export const checkPushPermissionsIOS = async () => {
-  if (isAndroid) {
-    return true;
-  }
+export const checkNotificationPermissions = async () => {
   try {
-    const status = await Notifications.ios.checkPermissions();
-    if (!status.notificationCenter) {
-      Alert.alert(i18n.t('notifications.permissionsError'), '', [
-        {
-          text: i18n.t('location.goToSettings'),
-          onPress: () => {
-            openSettings();
-          },
-        },
-        {text: i18n.t('common.cancel'), onPress: () => {}},
-      ]);
+    const settings = await notifee.requestPermission();
+
+    if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
+      console.log('User granted notification permissions');
+    } else {
+      console.log('User declined notification permissions');
     }
   } catch (error) {
     console.log(error);
@@ -87,57 +57,79 @@ export const checkPushPermissionsIOS = async () => {
 };
 
 export const invokeGetToken = async () => {
-  // Request permissions on iOS, refresh token on Android
-  Notifications.registerRemoteNotifications();
+  if (isAndroid) {
+    messaging().registerDeviceForRemoteMessages();
+  }
 };
 
-//TODO check if we need those id's for all types , maybe we can merge some together with handlers
-/*
- we can use ids for clearing notification from Notification Center
- */
-const localNotificationsIDs = {
-  [NotificationTypesEnum.EmergencyAreYouOk]: {
-    id: 987654321,
-  },
-  [NotificationTypesEnum.EmergencyOffline]: {
-    id: 987654322,
-  },
-  [NotificationTypesEnum.EmergencyRegularCheck]: {
-    id: 987654323,
-  },
-  [NotificationTypesEnum.EmergencyAlert]: {
-    id: 654456987,
-  },
+const handleForegroundState = (isRegularCheck: boolean) => {
+  if (AppState.currentState === 'active') {
+    let params;
+    isRegularCheck
+      ? (params = {regularCheck: true})
+      : (params = {healthCheck: true});
+    // @ts-ignore
+    navigationRef.navigate(Screens.HealthConditionError as never, params);
+  }
 };
 
-type NotificationPayload = {
-  type: Exclude<
-    NotificationTypesEnum,
-    NotificationTypesEnum.TimeSlotNotification
-  >;
-  body: string;
-  title: string;
-  additionalPayload?: Record<string, unknown>;
-};
+export const handleRemoteMessages = async (message: {
+  data: any;
+  notification: any;
+}) => {
+  const {data, notification} = message;
+  const {type} = data;
+  const {title, body} = notification;
 
-export const pushLocalNotification = ({
-  type,
-  title,
-  body,
-  ...rest
-}: NotificationPayload) => {
-  const values = localNotificationsIDs[type];
-  Notifications.postLocalNotification(
-    {
-      title: EnvConfig.DEV
-        ? `[DEBUG] ${title} ${new Date().toLocaleTimeString()}`
-        : title,
-      body,
-      payload: {
-        type: type,
-        ...rest,
-      },
-    } as Notification,
-    values.id,
-  );
+  if (
+    [
+      NotificationTypesEnum.EmergencyRegularCheck,
+      NotificationTypesEnum.EmergencyHealthCheck,
+      NotificationTypesEnum.EmergencyAlert,
+    ].includes(type)
+  ) {
+    const user = await AsyncStorageService.getItem(
+      AsyncStorageEnum.PersistedUserSettings,
+    );
+    const userParsed = user && (await JSON.parse(user));
+    userParsed.regularPushNotifications &&
+      (await updateNotification(
+        'Emergency triggered remotely',
+        'Click to cancel it',
+      ));
+    await AsyncStorageService.setItem(
+      AsyncStorageEnum.IsEmergencyEscalationStarted,
+      'true',
+    );
+  }
+  switch (type) {
+    case NotificationTypesEnum.EmergencyAlert:
+      await AsyncStorageService.setItem(AsyncStorageEnum.HealthTrigger, 'true');
+      handleForegroundState(false);
+      await soundNotification();
+      if (isIOS) {
+        Vibration.vibrate([1300, 2000], true);
+      }
+      await updateLocation();
+      // android to show the healthConditionScreen for the app from the lock screen of the mobile
+      updateNotification(title, body, type);
+      break;
+    case NotificationTypesEnum.EmergencyHealthCheck:
+      //@ts-ignore
+      await AsyncStorageService.setItem(AsyncStorageEnum.HealthTrigger, 'true');
+      handleForegroundState(false);
+      break;
+    case NotificationTypesEnum.EmergencyRegularCheck:
+      //@ts-ignore
+      await AsyncStorageService.setItem(AsyncStorageEnum.TimeTrigger, 'true');
+      handleForegroundState(true);
+      break;
+    case NotificationTypesEnum.TimeSlotNotification:
+      // ToastService.success(`${title}\n${body}`, {
+      //   visibilityTime: 5000,
+      // });
+      break;
+    default:
+      console.log('Notification Listener: unhandled notification');
+  }
 };
