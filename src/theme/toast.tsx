238182e63f0c2f30aac8f1d7c4f6b265988a027c
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-06 10:03:54 +0200

    Modified toast messages the will be shown inside the application

--- src/theme/toast.tsx
+++ src/theme/toast.tsx
@@ -1,16 +1,77 @@
 import React from 'react';
-import {Box} from 'native-base';
-import Alert from '~/components/Alert';
+import Toast, {BaseToast} from 'react-native-toast-message';
+import colors from './colors';
+import {StyleSheet} from 'react-native';
 
 export const toastConfig = {
   biostasis_success: ({text1}: {text1: string}) => (
-    <Box alignSelf="stretch" m={5}>
-      <Alert label={text1} />
-    </Box>
+    <BaseToast
+      text1={text1}
+      style={{
+        ...styles.main,
+        borderLeftColor: colors.green[200],
+        backgroundColor: colors.green[200],
+      }}
+      contentContainerStyle={styles.container}
+      onTrailingIconPress={() => Toast.hide()}
+      text1Style={styles.text1}
+      text1NumberOfLines={4}
+      text2NumberOfLines={0}
+    />
   ),
   biostasis_error: ({text1}: {text1: string}) => (
-    <Box alignSelf="stretch" m={5}>
-      <Alert label={text1} error />
-    </Box>
+    <BaseToast
+      text1={text1}
+      style={{
+        ...styles.main,
+        borderLeftColor: colors.red[200],
+        backgroundColor: colors.red[200],
+      }}
+      contentContainerStyle={styles.container}
+      onTrailingIconPress={() => Toast.hide()}
+      text1Style={styles.text1}
+      text1NumberOfLines={4}
+      text2NumberOfLines={0}
+    />
+  ),
+  biostasis_warning: ({text1}: {text1: string}) => (
+    <BaseToast
+      text1="Warning"
+      text2={text1}
+      style={{
+        ...styles.main,
+        borderLeftColor: colors.yellow[600],
+        backgroundColor: colors.yellow[600],
+      }}
+      contentContainerStyle={styles.container}
+      onTrailingIconPress={() => Toast.hide()}
+      text2Style={styles.text1}
+      text1Style={styles.text2}
+      text1NumberOfLines={1}
+      text2NumberOfLines={4}
+    />
   ),
 };
+
+const styles = StyleSheet.create({
+  main: {
+    height: undefined,
+    padding: 5,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text1: {
+    fontSize: 15,
+    fontWeight: '400',
+    color: colors.white,
+    textAlign: 'center',
+  },
+  text2: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: colors.white,
+    textAlign: 'center',
+  },
+});
