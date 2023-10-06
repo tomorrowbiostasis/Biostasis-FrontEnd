4458702fe573cc205440a6860afec1587b58c4ce
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-10-06 10:02:32 +0200

    Modified the headers for different naviagtor stacks

--- src/theme/navigators.ts
+++ src/theme/navigators.ts
@@ -1,46 +1,27 @@
 import {StackNavigationOptions} from '@react-navigation/stack';
-import colors from '~/theme/colors';
-import DrawerTrigger from '~/components/DrawerTrigger';
-import BackButtonIcon from '~/components/assets/Icons/BackButtonIcon';
 import LogoutTrigger from '~/components/LogoutTrigger';
+import colors from './colors';
 
-export const commonScreenOptions = {
+export const headerStyle: StackNavigationOptions = {
   headerShown: true,
+  headerTitle: '',
   headerStyle: {
-    backgroundColor: colors.gray['200'],
+    backgroundColor: colors.gray[50],
     elevation: 0,
     shadowOpacity: 0,
-    height: 110,
+    height: 100,
   },
-  headerLeftContainerStyle: {
-    paddingLeft: 20,
-  },
-  headerTitle: '',
-  headerBackTitleVisible: true,
-  headerBackImage: BackButtonIcon,
-  headerBackTitle: 'Back', // TODO: take value from i18next
-  headerBackTitleStyle: {marginLeft: 22},
-  headerTintColor: colors.gray['800'],
 };
 
-export const homeScreenOptions = {
-  ...commonScreenOptions,
-  headerRightContainerStyle: {
-    paddingRight: 20,
-  },
-  headerRight: DrawerTrigger,
-};
-
-export const welcomeScreenOptions = {
-  headerTitle: 'Welcome', // TODO: take default value from i18next
-  headerTitleAlign: 'left',
-  headerTitleStyle: {fontWeight: '400', fontSize: 18, fontFamily: 'Roboto'},
-};
-
-export const noHamburgerScreenOptions = {
-  headerRight: null,
+export const homeScreenOptions: StackNavigationOptions = {
+  headerShown: false,
 };
 
 export const logoutScreenOptions: StackNavigationOptions = {
-  headerRight: LogoutTrigger,
+  headerLeft: LogoutTrigger,
+  ...headerStyle,
+  headerLeftContainerStyle: {
+    position: 'absolute',
+    padding: 20,
+  },
 };
